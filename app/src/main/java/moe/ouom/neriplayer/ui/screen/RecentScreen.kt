df36fa4c3db66e7160c559844343b2b7ca05e7c1
cwuom <cwuomcwuom@163.com>
2026-01-11 05:39:22 +0800

    ui: remove redundant HorizontalDivider in RecentScreen
    - Remove `HorizontalDivider` from the list of recently added items to streamline the UI.

--- app/src/main/java/moe/ouom/neriplayer/ui/screen/RecentScreen.kt
+++ app/src/main/java/moe/ouom/neriplayer/ui/screen/RecentScreen.kt
@@ -291,7 +291,6 @@ fun RecentScreen(
                             }
                         }
                     )
-                    HorizontalDivider()
                 }
             }
         }
