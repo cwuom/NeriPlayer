e92dffff74ea359684b2d79cc7830c1428632617
cwuom <cwuomcwuom@163.com>
2026-03-13 19:59:35 +0800

    fix: adjust local song copy clipboard

--- app/src/main/java/moe/ouom/neriplayer/ui/component/LocalSongDialogs.kt
+++ app/src/main/java/moe/ouom/neriplayer/ui/component/LocalSongDialogs.kt
@@ -1,5 +1,6 @@
 package moe.ouom.neriplayer.ui.component
 
+import android.content.ClipData
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -21,10 +22,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
@@ -61,7 +62,7 @@ fun LocalSongDetailsDialog(
         title = { Text(stringResource(R.string.local_song_details_title)) },
         text = {
             fun copyPath(path: String) {
-                clipboardManager.setText(AnnotatedString(path))
+                clipboardManager.setClip(ClipEntry(ClipData.newPlainText("text", path)))
                 Toast.makeText(context, context.getString(R.string.toast_copied), Toast.LENGTH_SHORT).show()
             }
 
